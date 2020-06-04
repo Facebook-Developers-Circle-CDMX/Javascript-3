c1497a1aff2c2ae25ad07bee0ff840867ae3665a
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-06-04 14:30:30 -0500

    adding solution

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const findDouble = function (number) {
     //aqui escribiras tu codigo
-}; 
+    var temp = []
+    for (let index = 0; index < number.length; index++) {
+        if (temp.includes(number[index])) {
+            return number[index];
+        } else {
+            temp.push(number[index]);
+        }
+    }
+};
 
 module.exports = findDouble;
