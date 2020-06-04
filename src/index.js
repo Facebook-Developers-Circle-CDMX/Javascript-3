00c113baad88c1974ca381bd0d8b98e52d5416b4
enrique26 <enriqueibilis@gmail.com>
2020-06-04 17:28:26 -0500

    encontrar la primera dupla
    encontrar la primera dupla

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const findDouble = function (number) {
     //aqui escribiras tu codigo
+    var lid = []
+    
+    for(number of arr){
+        
+        if(lid.indexOf(number) == -1){
+            lid.push(number)
+        }else{
+            return number
+        }
+    }
+    return null
 }; 
 
 module.exports = findDouble;
