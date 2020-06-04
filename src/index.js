323613fa5a54c5951c3f7fde86d9ee5bfc4b2689
Emmanuel Santos <e.ss.rdz@gmail.com>
2020-06-04 15:02:26 -0500

    Desafio completo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const findDouble = function (number) {
     //aqui escribiras tu codigo
+    const mySet=new Set();
+    for(let i=0;i<=number.length;i++){
+	    if(mySet.has(number[i])){
+  	        return number[i]
+        }else{
+  	        mySet.add(number[i]);
+        }
+    }
 }; 
 
 module.exports = findDouble;
