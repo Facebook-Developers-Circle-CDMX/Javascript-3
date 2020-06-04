bcd74da6632325c3cdadfc322416d9f9c73850d3
Marco Tzuc <mirzoz12@gmail.com>
2020-06-04 18:10:18 -0500

    Add function and fix test

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
-const findDouble = function (number) {
-    //aqui escribiras tu codigo
-}; 
+const findDouble = function (numbers) {
+  var array = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (array.includes(numbers[i])) return numbers[i];
+
+    array.push(numbers[i]);
+  }
+};
 
 module.exports = findDouble;
